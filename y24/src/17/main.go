715fd1ef8ce13a3181b31227d2e30681a2593ef3
David Riddervold Marconis <david@marconis.no>
2025-01-03 09:28:06 +0100

    Add 2024 day 17 p2 solution

--- y24/src/17/main.go
+++ y24/src/17/main.go
@@ -26,23 +26,39 @@ func main() {
 	for i, n := range sp {
 		program[i], _ = strconv.Atoi(n)
 	}
-	var output = run(a, b, c, program)
+	var output = run(a, b, c, program, false)
 	// Jesus chris Go...
 	fmt.Println(strings.Trim(strings.Replace(fmt.Sprint(output), " ", ",", -1), "[]"))
+	l := 4
+	d := 1
+	for i := 0; i < int(math.Pow(8, float64(len(program)))); i += d {
+		output = run(i, 0, 0, program, true)
+		if len(output) > l {
+			l++
+			d *= 8
+		}
+		if len(output) != len(program) {
+			continue
+		}
+		fmt.Println(i)
+		break
+	}
 }
 
-func run(a, b, c int, program []int) []int {
+func run(a, b, c int, program []int, selfValidate bool) []int {
 	output := []int{}
 	pc := 0
+	z, o, t, e := 0, 1, 2, 3
+	r := [7]*int{&z, &o, &t, &e, &a, &b, &c}
 	for pc < len(program) {
 		in, op := program[pc], program[pc+1]
 		switch in {
 		case 0:
-			a = a / int(math.Pow(2, float64(combo(op, a, b, c))))
+			a = a / int(math.Pow(2, float64(*r[op])))
 		case 1:
 			b = b ^ op
 		case 2:
-			b = combo(op, a, b, c) % 8
+			b = *r[op] % 8
 		case 3:
 			if a != 0 {
 				pc = op
@@ -51,18 +67,16 @@ func run(a, b, c int, program []int) []int {
 		case 4:
 			b = b ^ c
 		case 5:
-			output = append(output, combo(op, a, b, c)%8)
+			output = append(output, *r[op]%8)
+			if selfValidate && output[len(output)-1] != program[len(output)-1] {
+				return output[:len(output)-1]
+			}
 		case 6:
-			b = a / int(math.Pow(2, float64(combo(op, a, b, c))))
+			b = a / int(math.Pow(2, float64(*r[op])))
 		case 7:
-			c = a / int(math.Pow(2, float64(combo(op, a, b, c))))
+			c = a / int(math.Pow(2, float64(*r[op])))
 		}
 		pc += 2
 	}
 	return output
-}
-
-func combo(op, a, b, c int) int {
-	r := [7]int{0, 1, 2, 3, a, b, c}
-	return r[op]
 }
